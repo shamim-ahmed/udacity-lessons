a156fb17474c6516df7268b7bc6d114d14018868
Shamim Ahmed <shamim.buet.99@gmail.com>
2015-08-30 16:59:21 +1000

    implemented simple functionality that uses grid view

--- GridViewExample/app/src/main/java/edu/udacity/android/gridviewexample/MainActivity.java
+++ GridViewExample/app/src/main/java/edu/udacity/android/gridviewexample/MainActivity.java
@@ -4,35 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+  private static final String[] values =
+      {"A", "B", "C", "D", "E",
+          "F", "G", "H", "I", "J",
+          "K", "L", "M", "N", "O",
+          "P", "Q", "R", "S", "T",
+          "U", "V", "W", "X", "Y", "Z"};
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-  }
 
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
+    GridView gridView = (GridView) findViewById(R.id.grid_view);
+    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.text_view, values);
+    gridView.setAdapter(arrayAdapter);
   }
 }
