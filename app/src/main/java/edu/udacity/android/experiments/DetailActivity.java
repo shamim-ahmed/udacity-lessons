0fcbdf1a488b84168a0c6475cac6d9981685f45d
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2015-08-30 10:48:52 +1000

    added the code that I wrote as part of going through various lessons

---
+++ app/src/main/java/edu/udacity/android/experiments/DetailActivity.java
@@ -0,0 +1,86 @@
+package edu.udacity.android.experiments;
+
+import android.content.Intent;
+import android.media.audiofx.BassBoost;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DetailActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bundle extras = getIntent().getExtras();
+        String forecast = null;
+
+        if (extras != null) {
+            forecast = extras.getString("FORECAST_DATA");
+        }
+
+        setContentView(R.layout.activity_detail);
+        if (savedInstanceState == null) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            fragment.setForecast(forecast);
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, fragment)
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        private String forecast;
+
+        public PlaceholderFragment() {
+        }
+
+        public void setForecast(String forecast) {
+            this.forecast = forecast;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+            TextView forecastView = (TextView) rootView.findViewById(R.id.forecast);
+            forecastView.setText(forecast);
+            return rootView;
+        }
+    }
+}
---
+++ app/src/main/java/edu/udacity/android/experiments/MainActivity.java
@@ -0,0 +1,81 @@
+package edu.udacity.android.experiments;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_forecast);
+
+        LinearLayout rootLayout = (LinearLayout) findViewById(R.id.linear_layout);
+
+        ListView listView = (ListView) rootLayout.findViewById(R.id.forecast_listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, R.layout.list_item, R.id.forecast_textView, new ArrayList<String>());
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ArrayAdapter<String> arp = (ArrayAdapter<String>)((ListView) parent).getAdapter();
+                String data = arp.getItem(position);
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                intent.putExtra("FORECAST_DATA", data);
+                startActivity(intent);
+                //Toast.makeText(parent.getContext(), data, Toast.LENGTH_LONG).show();
+            }
+        });
+        fetchForecastData(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+
+        if (id == R.id.refresh_settings) {
+            ListView listView = (ListView) findViewById(R.id.forecast_listView);
+            fetchForecastData((ArrayAdapter<String>) listView.getAdapter());
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void fetchForecastData(ArrayAdapter<String> adapter) {
+        ForecastDownloadTask task = new ForecastDownloadTask(adapter);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = prefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+        String[] values = location.split(",");
+        task.execute(values[0], values[1]);
+    }
+}
