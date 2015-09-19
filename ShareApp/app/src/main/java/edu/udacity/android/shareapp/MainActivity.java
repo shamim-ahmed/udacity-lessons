2ed1c32fc752309bfb063c2857c2d6e409422da6
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2015-09-19 22:24:09 +1000

    added log statements to investigate various lifecycle events

--- ShareApp/app/src/main/java/edu/udacity/android/shareapp/MainActivity.java
+++ ShareApp/app/src/main/java/edu/udacity/android/shareapp/MainActivity.java
@@ -5,15 +5,44 @@ import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate() called.....");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestory() called.....");
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart() called.....");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop() called.....");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause() called.....");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume() called.....");
     }
 
     @Override
