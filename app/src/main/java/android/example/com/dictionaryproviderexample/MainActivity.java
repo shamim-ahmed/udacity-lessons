27dc9740a9014f95f34da189f6cdced1b0a4250e
Shamim Ahmed <shamim.buet.99@gmail.com>
2015-09-27 00:05:37 +1000

    now the result is displayed using a CursorAdapter

--- app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
+++ app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
@@ -21,7 +21,7 @@ import android.os.Bundle;
 import android.provider.UserDictionary;
 import android.provider.UserDictionary.Words;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.TextView;
+import android.widget.ListView;
 
 /**
  * This is the central activity for the Provider Dictionary Example App. The purpose of this app is
@@ -36,28 +36,14 @@ public class MainActivity extends ActionBarActivity {
 
         // Get the ContentResolver which will send a message to the ContentProvider
         ContentResolver resolver = getContentResolver();
-
-        StringBuilder resultBuilder = new StringBuilder();
-
-        try (Cursor cursor = resolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null)) {
-            int n = cursor.getCount();
-            resultBuilder.append(String.format("The UserDictionary contains %d words%n", n));
-            resultBuilder.append("COLUMNS: _id - frequency - word\n");
-
-            while (cursor.moveToNext()) {
-                int idIndex = cursor.getColumnIndex(Words._ID);
-                int frequencyIndex = cursor.getColumnIndex(Words.FREQUENCY);
-                int wordIndex = cursor.getColumnIndex(Words.WORD);
-
-                int id = cursor.getInt(idIndex);
-                int frequency = cursor.getInt(frequencyIndex);
-                String word = cursor.getString(wordIndex);
-
-                resultBuilder.append(id).append(" - ").append(frequency).append(" - ").append(word).append("\n");
-            }
-        }
-
-        TextView textView = (TextView) findViewById(R.id.dictionary_text_view);
-        textView.setText(resultBuilder.toString());
+        ListView listView = (ListView) findViewById(R.id.dictionary_list_view);
+        Cursor cursor = resolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null);
+        MyCursorAdapter adapter = new MyCursorAdapter(this,
+                android.R.layout.two_line_list_item,
+                cursor,
+                new String[]{Words.WORD, Words.FREQUENCY},
+                new int[]{android.R.id.text1, android.R.id.text2},
+                0);
+        listView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/android/example/com/dictionaryproviderexample/MyCursorAdapter.java
@@ -0,0 +1,11 @@
+package android.example.com.dictionaryproviderexample;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.widget.SimpleCursorAdapter;
+
+public class MyCursorAdapter extends SimpleCursorAdapter {
+    public MyCursorAdapter(Context context, int layout, Cursor c, String[] from, int[] to, int flags) {
+        super(context, layout, c, from, to, flags);
+    }
+}
